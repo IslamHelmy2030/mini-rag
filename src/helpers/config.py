249d418a0915b4e09db7aef310b1dba1f61edcd3
IslamHelmy <Islam.Helmy@Tharwah.net>
2025-10-04 22:15:35 +0300

    Introduce MongoDB integration and database schema models
    - Added `Project` and `DataChunk` models with validation using `pydantic`.
    - Enabled MongoDB connection management in `main.py` with FastAPI lifecycle events.
    - Updated `config` to include MongoDB settings (`MONGODB_URL`, `MONGODB_DATABASE`).
    - Added `docker-compose.yml` for MongoDB setup with Docker.
    - Extended `.gitignore` to exclude MongoDB-related files.
    - Updated dependencies to include `motor` for MongoDB interactions.

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -6,10 +6,13 @@ class Settings(BaseSettings):
     app_name: str
     app_version: str
     openai_api_key: str
+
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE: int
 
+    MONGODB_URL:str
+    MONGODB_DATABASE:str
 
 
     model_config = SettingsConfigDict(env_file=".env")
--- src/main.py
+++ src/main.py
@@ -1,10 +1,32 @@
 from fastapi import FastAPI
 from routes import base, data
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers.config import get_settings
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup_event():
+    app.mongodb_client = AsyncIOMotorClient(get_settings().MONGODB_URL)
+    app.mongodb = app.mongodb_client[get_settings().MONGODB_DATABASE]
+
+@app.on_event("shutdown")
+async def shutdown_db_connection():
+    app.mongodb_client.close()
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     import uvicorn
